a7885986a207c862394bcbec3fcffda0541eb343
snpatil1999 <57366890+snpatil1999@users.noreply.github.com>
2021-06-22 12:55:43 +0530

    Update ShiftElementsInArray.java

--- src/main/java/exam/ShiftElementsInArray.java
+++ src/main/java/exam/ShiftElementsInArray.java
@@ -3,7 +3,32 @@ package exam;
 public class ShiftElementsInArray {
 
   public void shift(int[] arr) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
+      int last;
+    last=total[k-1];
+    for(int i=no-1;i>0;i++)
+    {
+        total[i]=total[i-1];
+    }
+    total[0] = last;
+    for(int i=0;i<no;i++)
+    {
+        System.ou.println(total[i]+"");
+    }
+    return 0;
+  }
+  public static void main(String[] args){
+      int no;
+      int[] arr=new int[100];
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the no of element you want to enter :");
+      no = sc.nextLine();
+      System.out.println("Enter the numbers :");
+      for(int i=0;i<n;i++)
+      {
+         arr[i]=sc.nextLine();
+      }
+    getNumberOfWays(arr,no);
+  }
 
   }
 
